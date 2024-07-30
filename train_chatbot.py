2e6ff871b979d84f56796edef8212ca19291e3dd
dwayne.laughlin <dwayne.laughlin@fdmgroup.com>
2024-07-30 16:11:24 -0500

    added tokenization to training file and inserted stub data for intents.json

--- train_chatbot.py
+++ train_chatbot.py
@@ -7,3 +7,19 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 import json
 import pickle
+
+lemmatizer = WordNetLemmatizer()
+words = []
+classes = []
+documents = []
+ignore_letters = ['!', '?', '.', ',']
+intents_file = open('intents.json').read()
+intents = json.loads(intents_file)
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word = nltk.tokenize(pattern)
+        words.extend(word)
+        documents.append(intent['tag'])
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
