97a89898b8496e32ca85ff090c404cfe3a3b348a
dwayne.laughlin <dwayne.laughlin@fdmgroup.com>
2024-08-02 19:09:29 -0500

    added training logic and model

--- train_chatbot.py
+++ train_chatbot.py
@@ -7,10 +7,10 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 import json
 import pickle 
-with open("Classes.pkl", "wb") as classes:
-    classes_file = pickle.load(classes)
-with open("Words.pkl", "wb") as words:
-    words_file = pickle.load(words)
+with open("Classes.pkl", "wb") as classesFile:
+    classes_file = pickle.load(classesFile)
+with open("Words.pkl", "wb") as wordsFile:
+    words_file = pickle.load(wordsFile)
 
 
 
@@ -45,3 +45,43 @@ print(len(words), "unique lemmatized words", words)
 # dump saves the python object in a file
 pickle.dump(words, words_file)
 pickle.dump(classes, classes_file)
+
+# creating training data
+training = []
+output_empty = [0] * len(classes)
+
+for doc in documents:
+    bag = []
+    pattern_words = doc[0]
+    lemmatizer.lemmatize(word.lower() for word in pattern_words)
+    for word in words:
+        bag.append(1) if word in pattern_words else bag.append(0)
+    
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append(bag, output_row)
+
+    random.shuffle(training)
+    training = np.array(training)
+
+    train_patterns = list(training[:,0])
+    train_intents = list(training[:,1])
+    print("Training data created")
+
+    model = Sequential()
+    model.add(Dense(128, input_shape=(len(train_patterns[0]),), activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(train_intents[0]),activation='softmax'))
+
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+    hist = model.fit(np.array(train_patterns), np.array(train_intents), epochs=200, batch_size=5, verbose=1)
+
+    model.save('chatbot_model.h5', hist)
+
+    print("model created")
+
